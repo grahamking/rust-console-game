f9208fda9069f8a5cb40d471edf0c1e9567b8e68
Graham King <graham@gkgk.org>
2021-05-16 16:55:51 -0700

    characters move

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::io::{stdout, Stdout, Write};
 use std::sync::{Arc, Mutex};
 use std::{thread, time};
 
+#[derive(Copy, Clone)]
 enum Dir {
     Up,
     Down,
@@ -12,9 +13,51 @@ enum Dir {
     Right,
 }
 
-enum Player {
-    One,
-    Two,
+#[derive(Copy, Clone)]
+struct Pos {
+    x: u16,
+    y: u16,
+}
+impl Pos {
+    fn mov(&mut self, dir: Dir) {
+        match dir {
+            Dir::Up => {
+                if self.y > 0 {
+                    self.y -= 1
+                }
+            }
+            Dir::Down => self.y += 1,
+            Dir::Left => {
+                if self.x > 0 {
+                    self.x -= 1
+                }
+            }
+            Dir::Right => self.x += 1,
+        }
+    }
+    fn reverse(&mut self, dir: Dir) -> Dir {
+        let new_dir = match dir {
+            Dir::Up => Dir::Down,
+            Dir::Down => Dir::Up,
+            Dir::Left => Dir::Right,
+            Dir::Right => Dir::Left,
+        };
+        self.mov(new_dir);
+        new_dir
+    }
+}
+
+struct Character {
+    pos: Pos,
+    dir: Dir,
+}
+impl Character {
+    fn new(x: u16, y: u16, dir: Dir) -> Character {
+        Character {
+            pos: Pos { x, y },
+            dir,
+        }
+    }
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
@@ -25,32 +68,43 @@ fn main() -> Result<(), Box<dyn Error>> {
 }
 
 struct Game {
-    stdout: Arc<Mutex<Stdout>>,
+    data: Arc<GameData>,
+}
+
+struct GameData {
     w: u16,
     h: u16,
+    stdout: Mutex<Stdout>,
+    players: Mutex<(Character, Character)>,
 }
 
 impl Game {
     fn new() -> Result<Game, Box<dyn Error>> {
         let (w, h) = terminal::size()?;
-        let g = Game {
-            stdout: Arc::new(Mutex::new(stdout())),
+        let quarter = w / 4;
+        let gd = GameData {
             w,
             h,
+            stdout: Mutex::new(stdout()),
+            players: Mutex::new((
+                Character::new(quarter, h / 2, Dir::Right),
+                Character::new(quarter * 3, h / 2, Dir::Left),
+            )),
         };
+        let g = Game { data: Arc::new(gd) };
 
         terminal::enable_raw_mode()?;
         queue!(
-            g.stdout.lock().unwrap(),
+            g.data.stdout.lock().unwrap(),
             terminal::Clear(terminal::ClearType::All),
             cursor::Hide,
             cursor::MoveTo(0, 0),
         )?;
 
-        draw_board(g.stdout.clone())?;
+        g.draw_board()?;
 
-        let thread_stdout = g.stdout.clone();
-        let _ = thread::spawn(move || mover(thread_stdout));
+        let gd2 = g.data.clone();
+        let _ = thread::spawn(move || draw_loop(gd2));
 
         Ok(g)
     }
@@ -63,20 +117,19 @@ impl Game {
                     KeyCode::Char('q') => break 'top,
 
                     // player one keys
-                    KeyCode::Up => self.mov(Player::One, Dir::Up),
-                    KeyCode::Down => self.mov(Player::One, Dir::Down),
-                    KeyCode::Left => self.mov(Player::One, Dir::Left),
-                    KeyCode::Right => self.mov(Player::One, Dir::Right),
-                    KeyCode::Char('m') => self.fire(Player::One),
+                    KeyCode::Char('w') => self.data.players.lock().unwrap().0.dir = Dir::Up,
+                    KeyCode::Char('s') => self.data.players.lock().unwrap().0.dir = Dir::Down,
+                    KeyCode::Char('a') => self.data.players.lock().unwrap().0.dir = Dir::Left,
+                    KeyCode::Char('d') => self.data.players.lock().unwrap().0.dir = Dir::Right,
+                    //KeyCode::Tab => self.fire(Player::One),
 
                     // player two keys
-                    KeyCode::Char('w') => self.mov(Player::Two, Dir::Up),
-                    KeyCode::Char('s') => self.mov(Player::Two, Dir::Down),
-                    KeyCode::Char('a') => self.mov(Player::Two, Dir::Left),
-                    KeyCode::Char('d') => self.mov(Player::Two, Dir::Right),
-                    KeyCode::Tab => self.fire(Player::Two),
-
-                    _ => println!("KEY: {:?}", e.code),
+                    KeyCode::Up => self.data.players.lock().unwrap().1.dir = Dir::Up,
+                    KeyCode::Down => self.data.players.lock().unwrap().1.dir = Dir::Down,
+                    KeyCode::Left => self.data.players.lock().unwrap().1.dir = Dir::Left,
+                    KeyCode::Right => self.data.players.lock().unwrap().1.dir = Dir::Right,
+                    //KeyCode::Char('m') => self.fire(Player::Two),
+                    _ => (),
                 }
             }
         }
@@ -84,68 +137,87 @@ impl Game {
         Ok(())
     }
 
-    fn mov(&self, player: Player, dir: Dir) {
-        todo!();
-    }
+    fn draw_board(&self) -> Result<(), Box<dyn Error>> {
+        let (w, h) = terminal::size()?;
+        let mut stdout = self.data.stdout.lock().unwrap();
+        line(&mut *stdout, w)?;
+        let top = 0;
+        let bottom = h - 2;
 
-    fn fire(&self, player: Player) {
-        todo!();
+        for i in top + 1..bottom {
+            queue!(
+                stdout,
+                cursor::MoveTo(0, i),
+                style::Print("|"),
+                cursor::MoveTo(w - 1, i),
+                style::Print("|"),
+            )?;
+        }
+        queue!(stdout, cursor::MoveTo(0, bottom))?;
+        line(&mut *stdout, w)?;
+
+        // player start positions
+        let players = self.data.players.lock().unwrap();
+        queue!(
+            stdout,
+            cursor::MoveTo(players.0.pos.x, players.0.pos.y),
+            style::Print("1")
+        )
+        .unwrap();
+        queue!(
+            stdout,
+            cursor::MoveTo(players.1.pos.x, players.1.pos.y),
+            style::Print("2")
+        )
+        .unwrap();
+
+        stdout.flush()?;
+        Ok(())
     }
+}
+
+fn draw_loop(data: Arc<GameData>) {
+    let mut move_players = true;
+    loop {
+        let mut stdout = data.stdout.lock().unwrap();
+        let mut p_lock = data.players.lock().unwrap();
+        if move_players {
+            animate(&mut stdout, &mut p_lock.0, "1", data.w, data.h);
+            animate(&mut stdout, &mut p_lock.1, "2", data.w, data.h);
+        }
+        stdout.flush().unwrap();
+        drop(p_lock);
+        drop(stdout);
+        move_players = !move_players;
+        thread::sleep(time::Duration::from_millis(30));
+    }
+}
+
+fn animate(stdout: &mut Stdout, p: &mut Character, s: &str, w: u16, h: u16) {
+    queue!(stdout, cursor::MoveTo(p.pos.x, p.pos.y), style::Print(" "),).unwrap();
+    p.pos.mov(p.dir);
+    if !is_on_board(p, w, h) {
+        p.dir = p.pos.reverse(p.dir);
+    }
+    queue!(stdout, cursor::MoveTo(p.pos.x, p.pos.y), style::Print(s),).unwrap();
+}
+
+fn is_on_board(c: &Character, w: u16, h: u16) -> bool {
+    1 <= c.pos.x && c.pos.x < w - 1 && 1 <= c.pos.y && c.pos.y < h - 2
 }
 
 impl Drop for Game {
     fn drop(&mut self) {
-        let mut sl = self.stdout.lock().unwrap();
+        let mut sl = self.data.stdout.lock().unwrap();
         execute!(
             sl,
             terminal::Clear(terminal::ClearType::All),
-            cursor::MoveTo(0, self.h - 1),
+            cursor::MoveTo(0, self.data.h - 1),
             cursor::Show
         )
         .unwrap();
         terminal::disable_raw_mode().unwrap();
     }
-}
-
-// action thread
-fn mover(stdout: Arc<Mutex<Stdout>>) {
-    let (w, _h) = terminal::size().unwrap();
-    for i in 1..w - 2 {
-        let mut s_tmp = stdout.lock().unwrap();
-        queue!(s_tmp, cursor::MoveTo(i, 3), style::Print("1")).unwrap();
-        s_tmp.flush().unwrap();
-        drop(s_tmp);
-        thread::sleep(time::Duration::from_millis(30));
-        queue!(
-            stdout.lock().unwrap(),
-            cursor::MoveTo(i, 3),
-            style::Print(" ")
-        )
-        .unwrap();
-    }
-}
-
-fn draw_board(stdout: Arc<Mutex<Stdout>>) -> Result<(), Box<dyn Error>> {
-    let (w, h) = terminal::size()?;
-    let mut stdout = stdout.lock().unwrap();
-    line(&mut *stdout, w)?;
-    let top = 0;
-    let bottom = h - 2;
-
-    for i in top + 1..bottom {
-        queue!(
-            stdout,
-            cursor::MoveTo(0, i),
-            style::Print("|"),
-            cursor::MoveTo(w - 1, i),
-            style::Print("|"),
-        )?;
-    }
-    queue!(stdout, cursor::MoveTo(0, bottom))?;
-    line(&mut *stdout, w)?;
-    stdout.flush()?;
-
-    Ok(())
 }
 
 fn line<T: Write>(writer: &mut T, width: u16) -> Result<(), crossterm::ErrorKind> {
