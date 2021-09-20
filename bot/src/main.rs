57189ba7b3ce60f5f8e2d09745e34647cbdea53d
Graham King <graham@gkgk.org>
2021-09-20 11:23:17 -0700

    Move bot internals to the SDK

--- bot/src/main.rs
+++ bot/src/main.rs
@@ -1,24 +1,12 @@
 
 use std::env;
-use std::os::unix::net;
-use std::io::{Read, Write};
 use std::thread;
 use std::time;
-use std::convert::TryInto;
-use std::io::ErrorKind;
 use std::sync::{Arc, Mutex};
 use std::cmp::Ordering;
 
-use rs_sdk::{Dir, SOCK_NAME_1, SOCK_NAME_2};
+use rs_sdk::{connect, Dir, Player, SDKError};
 
-// must match the order in which players are added in src/lib.rs
-const PLAYER_1_ID: u8 = 0;
-const PLAYER_2_ID: u8 = 1;
-
-// Commands
-// Must match src/server.rs into_input_event
-const MOVE: u8 = 1;
-const FIRE: u8 = 2;
 
 const USAGE: &str = r#"Usage: bot 1|2
     1 to be player 1, 2 to be player 2. Defaults to player 1.
@@ -30,35 +18,28 @@ fn main() -> anyhow::Result<()> {
         anyhow::bail!("{}", USAGE);
     }
 
-    let (player_id, opponent_id, sock_path) = match args[0].as_str() {
-        "2" =>  (PLAYER_2_ID, PLAYER_1_ID, SOCK_NAME_2),
-        _ =>  (PLAYER_1_ID, PLAYER_2_ID, SOCK_NAME_1),
+    let (player, opponent) = match args[0].as_str() {
+        "2" =>  (Player::Two, Player::One),
+        _ =>  (Player::One, Player::Two),
     };
 
-    let mut sock_out = match net::UnixStream::connect(sock_path) {
-        Ok(s) => s,
-        Err(e) => anyhow::bail!("Couldn't connecto to {}. {}", sock_path, e),
-    };
-    let mut sock_in = sock_out.try_clone()?;
+    let (mut b_in, mut b_out) = connect(player)?;
+
     let target_dir_write = Arc::new(Mutex::new(Dir::None));
-
     let target_dir_read = target_dir_write.clone();
+
     let writer = thread::spawn(move || {
-        let mut move_cmd = vec![MOVE, 99, 0, 0, 0, 0, 0, 0];
-        let mut fire_cmd = vec![FIRE, 99, 0, 0, 0, 0, 0, 0];
         let mut is_move = true;
         loop {
-            let op_dir = target_dir_read.lock().unwrap().as_num();
-            if op_dir != 0 {
-                let cmd = if is_move {
-                    move_cmd[1] = op_dir;
-                    &move_cmd
+            let op_dir = *target_dir_read.lock().unwrap();
+            if op_dir != Dir::None {
+                let res = if is_move {
+                    b_out.dir(op_dir)
                 } else {
-                    fire_cmd[1] = op_dir;
-                    &fire_cmd
+                    b_out.fire(op_dir)
                 };
-                if let Err(e) = sock_out.write(cmd) {
-                    println!("bot sock write err: {}", e);
+                if let Err(e) = res {
+                    println!("Err sending command: {}", e);
                     return;
                 }
                 is_move = !is_move;
@@ -68,25 +49,25 @@ fn main() -> anyhow::Result<()> {
     });
 
     let reader = thread::spawn(move || {
-        let mut buf = [0u8; 12]; // protocol is units of 12 bytes
         let mut my_pos = (0, 0); // x,y of this bot
         let mut op_pos = (0, 0); // x,y of opponent
         loop {
-            if let Err(e) = sock_in.read_exact(&mut buf) {
-                match e.kind() {
-                    ErrorKind::UnexpectedEof => { // remote closed connection
+            let es = match b_in.get_next_entity() {
+                Ok(es) => es,
+                Err(err) => match err {
+                    SDKError::Stop => {
                         return;
                     },
-                    _ => {
-                        println!("bot read_exact: {}", e);
+                    SDKError::Misc(inner) => {
+                        println!("bot read_exact: {}", inner);
+                        return;
                     },
                 }
-            }
-            let es = EntityState::from_network(&buf);
-            if es.id == player_id {
-                my_pos = (es.x, es.y);
-            } else if es.id == opponent_id {
-                op_pos = (es.x, es.y);
+            };
+            if es.is_player(player) {
+                my_pos = es.pos();
+            } else if es.is_player(opponent) {
+                op_pos = es.pos();
             }
             let target_dir_1 = match my_pos.0.cmp(&op_pos.0) { // 0 is x
                 Ordering::Less => Dir::Right,
@@ -105,36 +86,6 @@ fn main() -> anyhow::Result<()> {
     writer.join().unwrap();
     reader.join().unwrap();
     Ok(())
-}
-
-#[derive(Debug)]
-struct EntityState {
-    id: u8,
-    x: u32,
-    y: u32,
-    dir: Dir,
-    velocity: u8,
-    has_shield: bool,
-}
-impl EntityState {
-    fn from_network(msg: &[u8; 12]) -> EntityState {
-        //println!("GOT: {:?}", msg);
-        let mut e = EntityState{
-            id: msg[0],
-            dir: Dir::from_num(msg[9]),
-            velocity: msg[10],
-            has_shield: msg[11] == 1,
-            x: 0,
-            y: 0,
-        };
-        // bytes 1..5 (not inclusive) are x position as u32
-        let (x_bytes, rest) = msg[1..].split_at(4);
-        e.x = u32::from_be_bytes(x_bytes.try_into().unwrap());
-        let (y_bytes, _) = rest.split_at(4); // next 4 bytes are y position as u32
-        e.y = u32::from_be_bytes(y_bytes.try_into().unwrap());
-
-        e
-    }
 }
 
 // if either are None return the other
--- rs-sdk/src/lib.rs
+++ rs-sdk/src/lib.rs
@@ -1,6 +1,169 @@
+use std::os::unix::net;
+use std::io::{Read, Write};
+use std::convert::TryInto;
+use std::io::ErrorKind;
+use std::error;
+use std::fmt;
 
 mod dir;
 pub use dir::Dir;
 
 pub const SOCK_NAME_1: &str = "/tmp/rust-console-game-p1.sock";
 pub const SOCK_NAME_2: &str = "/tmp/rust-console-game-p2.sock";
+
+// must match the order in which players are added in game/src/lib.rs
+const PLAYER_1_ID: u8 = 0;
+const PLAYER_2_ID: u8 = 1;
+
+// Commands
+// Must match game/src/server.rs into_input_event
+const MOVE: u8 = 1;
+const FIRE: u8 = 2;
+
+#[derive(Clone, Copy, Debug)]
+pub enum Player {
+    One,
+    Two,
+}
+impl Player {
+    fn sock_path(&self) -> &'static str {
+        match self {
+            Player::One => SOCK_NAME_1,
+            Player::Two => SOCK_NAME_2,
+        }
+    }
+    fn id(&self) -> u8 {
+        match self {
+            Player::One => PLAYER_1_ID,
+            Player::Two => PLAYER_2_ID,
+        }
+    }
+}
+
+pub struct BotIn {
+    sock_in: net::UnixStream,
+    buf: [u8; 12],  // protocol is units of 12 bytes
+}
+
+pub struct BotOut {
+    move_cmd: Vec<u8>,
+    fire_cmd: Vec<u8>,
+    sock_out: net::UnixStream,
+}
+
+pub fn connect(p: Player) -> Result<(BotIn, BotOut), anyhow::Error> {
+    let sp = p.sock_path();
+    let sock_out = match net::UnixStream::connect(sp) {
+        Ok(s) => s,
+        Err(e) => anyhow::bail!("Couldn't connecto to {}. {}", sp, e),
+    };
+    let sock_in = sock_out.try_clone()?;
+    let b_in = BotIn {
+        sock_in,
+        buf: [0u8; 12],
+    };
+    let b_out = BotOut {
+        sock_out,
+        move_cmd: vec![MOVE, 99, 0, 0, 0, 0, 0, 0],
+        fire_cmd: vec![FIRE, 99, 0, 0, 0, 0, 0, 0],
+    };
+    Ok((b_in, b_out))
+}
+
+impl BotOut {
+    // Set bot direction
+    pub fn dir(&mut self, d: Dir) -> Result<(), anyhow::Error> {
+        self.move_cmd[1] = d.as_num();
+        //self.send_cmd(&self.move_cmd)
+        match self.sock_out.write(&self.move_cmd) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(anyhow::anyhow!("socket write err: {}", e)),
+        }
+    }
+
+    // Fire in a direction
+    pub fn fire(&mut self, d: Dir) -> Result<(), anyhow::Error> {
+        self.fire_cmd[1] = d.as_num();
+        match self.sock_out.write(&self.fire_cmd) {
+            Ok(_) => Ok(()),
+            Err(e) => Err(anyhow::anyhow!("socket write err: {}", e)),
+        }
+    }
+
+    //fn send_cmd(&mut self, cmd: &[u8]) -> Result<(), anyhow::Error> {
+    //    match self.sock_out.write(cmd) {
+    //        Ok(_) => Ok(()),
+    //        Err(e) => Err(anyhow::anyhow!("socket write err: {}", e)),
+    //    }
+    //}
+}
+
+impl BotIn {
+    pub fn get_next_entity(&mut self) -> Result<EntityState, SDKError> {
+        if let Err(e) = self.sock_in.read_exact(&mut self.buf) {
+            match e.kind() {
+                ErrorKind::UnexpectedEof => { // remote closed connection
+                    return Err(SDKError::Stop);
+                },
+                _ => {
+                    return Err(SDKError::Misc(format!("bot read_exact: {}", e)));
+                },
+            }
+        }
+        Ok(EntityState::from_network(&self.buf))
+    }
+}
+
+#[derive(Debug)]
+pub enum SDKError {
+    Stop,
+    Misc(String),
+}
+
+impl error::Error for SDKError {}
+impl fmt::Display for SDKError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SDKError::Stop => write!(f, "stop"),
+            SDKError::Misc(msg) => write!(f, "{}", msg)
+        }
+    }
+}
+
+#[derive(Debug)]
+pub struct EntityState {
+    id: u8,
+    x: u32,
+    y: u32,
+    dir: Dir,
+    velocity: u8,
+    has_shield: bool,
+}
+impl EntityState {
+    fn from_network(msg: &[u8]) -> EntityState {
+        //println!("GOT: {:?}", msg);
+        let mut e = EntityState{
+            id: msg[0],
+            dir: Dir::from_num(msg[9]),
+            velocity: msg[10],
+            has_shield: msg[11] == 1,
+            x: 0,
+            y: 0,
+        };
+        // bytes 1..5 (not inclusive) are x position as u32
+        let (x_bytes, rest) = msg[1..].split_at(4);
+        e.x = u32::from_be_bytes(x_bytes.try_into().unwrap());
+        let (y_bytes, _) = rest.split_at(4); // next 4 bytes are y position as u32
+        e.y = u32::from_be_bytes(y_bytes.try_into().unwrap());
+
+        e
+    }
+
+    pub fn is_player(&self, p: Player) -> bool {
+        self.id == p.id()
+    }
+
+    pub fn pos(&self) -> (u32, u32) {
+        (self.x, self.y)
+    }
+}
